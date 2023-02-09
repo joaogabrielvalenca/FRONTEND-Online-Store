25bf3499bb899cef4a55ef5f7742f91ea30b027d
gabesouto <soutogabriel04@gmail.com>
2023-02-09 17:38:02 -0300

    Requisito 02 - concluído por Gabriel Souto

---
+++ src/component/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </div>
+    );
+  }
+}
+export default ProductList;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ProductList from '../component/ProductList';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <ProductList />
+      </div>
+    );
+  }
+}
+export default Home;
